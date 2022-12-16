cc92f19e6643d376f7a1532a132fa47c19f12b39
liipe2112 <lipe_amorim10@hotmail.com>
2022-12-16 00:29:01 -0300

    Resolução do desafio 1002 Java iniciante

---
+++ Bee1002.java
@@ -0,0 +1,33 @@
+package beecrowd.iniciante;
+
+import java.util.Scanner;
+
+/*
+ * QUESTÃO
+ * 
+	A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando para este problema que π = 3.14159:
+	- Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
+
+	Entrada
+	A entrada contém um valor de ponto flutuante (dupla precisão), no caso, a variável raio.
+
+	Saída
+	Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo, com 4 casas após o ponto decimal. 
+	Utilize variáveis de dupla precisão (double). Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, 
+	caso contrário, você receberá "Presentation Error". */
+
+public class Bee1002 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		/*Entradas */
+		double pi = 3.14159;
+		double raio = sc.nextDouble();
+		
+		/*Processamento */
+		double area = pi * (Math.pow(raio, 2));
+		
+		/*Saída */
+		System.out.println((String.format("A=%.4f", area)));
+	}
+}
